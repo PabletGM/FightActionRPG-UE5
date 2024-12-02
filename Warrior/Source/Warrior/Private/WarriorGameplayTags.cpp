65d2b9d0c3dddaeb20f97c37d6f35624be91db02
Pablo Garcia Moreno <pablogarciamoreno.pgm@gmail.com>
2024-12-02 23:47:49 +0100

    NativeGameplayTags created
    -class WarriorGameplayTags created

---
+++ Warrior/Source/Warrior/Private/WarriorGameplayTags.cpp
@@ -0,0 +1,12 @@
+// By Pablo Garcia
+
+
+#include "WarriorGameplayTags.h"
+
+
+namespace WarriorGameplayTags
+{
+	/** Input Tags **/
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_Mode,"InputTag.Move")
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_Look,"InputTag.Look")
+}
