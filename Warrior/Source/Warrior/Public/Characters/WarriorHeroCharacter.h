6aadc10de503e3b40ab2470c5e85076dd5078b49
Pablo Garcia Moreno <pablogarciamoreno.pgm@gmail.com>
2024-12-02 23:00:58 +0100

    BP_HeroCharacter and WarriorHeroCharacter prototype

--- Warrior/Source/Warrior/Public/Characters/WarriorHeroCharacter.h
+++ Warrior/Source/Warrior/Public/Characters/WarriorHeroCharacter.h
@@ -6,15 +6,32 @@
 #include "Characters/WarrriorBaseCharacter.h"
 #include "WarriorHeroCharacter.generated.h"
 
-/**
- * 
- */
+class USpringArmComponent;
+class UCameraComponent;
+
 UCLASS()
 class WARRIOR_API AWarriorHeroCharacter : public AWarrriorBaseCharacter
 {
 	GENERATED_BODY()
+
+public:
+	AWarriorHeroCharacter();
+
 protected:
 	virtual void BeginPlay() override;
+
+private:
+	
+	#pragma region Components
+	
+		UPROPERTY(VisibleAnywhere,BlueprintReadOnly, Category ="Camera", meta = (AllowPrivateAccess = "true"))
+		USpringArmComponent* CameraBoom;
+
+		UPROPERTY(VisibleAnywhere,BlueprintReadOnly, Category ="Camera", meta = (AllowPrivateAccess = "true"))
+		UCameraComponent* FollowCamera;
+	
+	#pragma endregion 
+
 	
 	
 };
