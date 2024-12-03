183dda10eebb47e231a6eb422462f18eab87048f
Pablo Garcia Moreno <pablogarciamoreno.pgm@gmail.com>
2024-12-03 17:17:36 +0100

    WarriorGameplayAbility
    WarriorGameplayAbility with methods like OnGiveAbility and EndEability override to add an ability and 2 types of abilityActivationPolicy

---
+++ Warrior/Source/Warrior/Private/AbilitySystem/Abilities/WarriorGameplayAbility.cpp
@@ -0,0 +1,37 @@
+// By Pablo Garcia
+
+
+#include "AbilitySystem/Abilities/WarriorGameplayAbility.h"
+#include "AbilitySystem/WarriorAbilitySystemComponent.h"
+
+void UWarriorGameplayAbility::OnGiveAbility(const FGameplayAbilityActorInfo* ActorInfo,
+	const FGameplayAbilitySpec& Spec)
+{
+	Super::OnGiveAbility(ActorInfo, Spec);
+
+	//what type of ability activation
+	if(AbilityActivationPolicy == EWarriorAbilityActivationPolicy::OnGiven)
+	{
+		//is the ability created
+		if(ActorInfo && !Spec.IsActive())
+		{
+			ActorInfo->AbilitySystemComponent->TryActivateAbility(Spec.Handle);
+		}
+	}
+}
+
+void UWarriorGameplayAbility::EndAbility(const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+	bool bReplicateEndAbility, bool bWasCancelled)
+{
+	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+
+	if(AbilityActivationPolicy == EWarriorAbilityActivationPolicy::OnGiven)
+	{
+		if(ActorInfo)
+		{
+			ActorInfo->AbilitySystemComponent->ClearAbility(Handle);
+		}
+	}
+	
+}
